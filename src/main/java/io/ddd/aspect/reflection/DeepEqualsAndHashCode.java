f6ed18971b7542568cdb97257bc6c90f83007323
repplix <michael.repplinger@dillinger.biz>
2020-03-06 13:58:50 +0100

    * cleanup class

--- src/main/java/io/ddd/aspect/reflection/DeepEqualsAndHashCode.java
+++ src/main/java/io/ddd/aspect/reflection/DeepEqualsAndHashCode.java
@@ -5,68 +5,88 @@ import java.util.Map;
 
 
 /**
- * Eine Klasse, die generische implementationen der {@link Object#equals(Object)} und {@link Object#hashCode()} Methoden bietet.
+ * This class provides generic methods to implement Java equals() and hashCode() methods according to their contracts.
+ *
+ * equals()
+ *   reflexive: an object must equal itself
+ *   symmetric: x.equals(y) must return the same result as y.equals(x)
+ *   transitive: if x.equals(y) and y.equals(z) then also x.equals(z)
+ *   consistent: the value of equals() should change only if a property that is contained in equals() changes (no randomness allowed)
+ *
+ *  hashCode()
+ *    internal consistency: the value of hashCode() may only change if a property that is in equals() changes
+ *    equals consistency: objects that are equal to each other must return the same hashCode
+ *    collisions: unequal objects may have the same hashCode
  */
 public class DeepEqualsAndHashCode
 {
-
     /**
-     * Privater konstruktor, da diese Klasse nur statische Methoden beinhaltet-
-     */
-    private DeepEqualsAndHashCode()
-    {
-    }
-
-
-    /**
-     * Vergleicht beide Objekte nach den gegebenheiten der {@link Object#equals(Object)} Methode.
-     * Diese Methode arbeitet mit reflection.
+     * This method can be used to implement a generic equals method.
      *
-     * @param object1 Das erste Objekt, das verglichen wird. Üblicherweise {@code this} beim aufruf.
+     * It compares if all attributes of the two given objects are equals
+     *
+     * @param object1 First given object which is comapr
      * @param object2 Das Objekt, welches mit dem ersten Objekt verglichen wird.
-     * @return True wenn beide Objekte gleich sind, False wenn nicht.
-     * @throws FieldIsArrayException Wenn eines der Attribute ein Array ist.
+     * @return True if all attributes of both objects are euqual
+     * @throws FieldIsArrayException If one of the two objects contains an array  
      */
     public static boolean isReflectiveEqual(final Object object1, final Object object2)
     {
-        if ( object1 == object2) {
+        if (isSameObject(object1, object2)) {
             return true;
         }
 
-        return (isBothNotNull(object1, object2) && isSameClass(object1, object2) && isDeepEquals(object1, object2));
+        if (!isBothNotNull(object1, object2)) {
+            return false;
+        }
+
+        if (!isSameClass(object1, object2)) {
+            return false;
+        }
+
+        return isDeepEquals(object1, object2);
     }
 
     /**
-     * Prüft ob die identität der Objekte die selbe ist.
+     * Hasht alle Attribute innerhalb eines Objekts und generiert einen Hashcode nach den richtlinien der {@link Object#hashCode()} Methode.
+     * Implementiert durch {@link Arrays#deepHashCode(Object[])}.
      *
-     * @param a Ein zu vergleichendes Objekt.
-     * @param b Ein anderes zu vergleichendes Objekt.
-     * @return True wenn beide das selbe Objekt sind.
+     * @param obj Das Objekt, wessen Hashcode generiert werden soll.
+     * @return Ein Hashcode basierend auf dem übergebenen Objekt.
      */
-    private static boolean isSame(final Object a, final Object b)
+    public static int reflectiveHashCode(final Object obj)
+    {
+        ClassAccessor objectAccessor = new ClassAccessor(obj);
+        Map<String, Object> attributes = objectAccessor.getAllClassAttributes();
+        Object[] attributeValues = attributes.values().toArray();
+        return Arrays.deepHashCode(attributeValues);
+    }
+
+    private DeepEqualsAndHashCode()
+    {
+    }
+    
+
+    private static boolean isSameObject(final Object a, final Object b)
     {
         return a == b;
     }
 
-    /**
-     * Überprüft ob beide Objekte der selben Klasse entsprechen.
-     *
-     * @param a Ein zu vergleichendes Objekt.
-     * @param b Ein anderes zu vergleichendes Objekt.
-     * @return True wenn beide Objekte der selben Klasse entsprechen.
-     */
     private static boolean isSameClass(final Object a, final Object b)
     {
         return a.getClass() == b.getClass();
     }
 
-    /**
-     * Vergleicht beide Objekte an hand ihrer Attribute mittels reflection. Berücksichtigt auch Oberklassen.
-     *
-     * @param a Ein zu vergleichendes Objekt.
-     * @param b Ein anderes zu vergleichendes Objekt.
-     * @return True, wenn beide Objekte die gleichen Attribute haben mit den gleichen werten.
-     */
+    private static boolean isSameSize(final Map<String, Object> attributesA, final Map<String, Object> attributesB)
+    {
+        return attributesA.size() == attributesB.size();
+    }
+
+    private static boolean isBothNotNull(final Object a, final Object b)
+    {
+        return a != null && b != null;
+    }
+
     private static boolean isDeepEquals(final Object a, final Object b)
     {
         Map<String, Object> attributesA = new ClassAccessor(a).getAllClassAttributes();
@@ -75,30 +95,11 @@ public class DeepEqualsAndHashCode
         return isDeepEquals(attributesA, attributesB);
     }
 
-
-    /**
-     * DeepEquals vergleich zweier Maps.
-     *
-     * @param attributesA Eine Map, die aus Attribut-Namen und Attribut-Werten eines Objekts besteht.
-     * @param attributesB Eine Map, die aus Attribut-Namen und Attribut-Werten eines Objekts besteht.
-     * @return True, wenn beide Maps gleich sind, false wenn nicht.
-     */
     private static boolean isDeepEquals(final Map<String, Object> attributesA, final Map<String, Object> attributesB)
     {
         return (isSameSize(attributesA, attributesB) && isAttributesGenericEquals(attributesA, attributesB));
     }
 
-    /**
-     * Vergleicht ob beide Maps die selbe größe haben.
-     *
-     * @param attributesA Eine zu vergleichende Map.
-     * @param attributesB Eine andere zu vergleichende Map.
-     * @return True, wenn beide Maps die selbe größe haben.
-     */
-    private static boolean isSameSize(final Map<String, Object> attributesA, final Map<String, Object> attributesB)
-    {
-        return attributesA.size() == attributesB.size();
-    }
 
     /**
      * Überprüft ob die einträge der beiden Maps jeweils gleich sind.
@@ -132,7 +133,7 @@ public class DeepEqualsAndHashCode
      */
     private static boolean isSingleAttributeGenericEquals(final Object a, final Object b)
     {
-        return (isSame(a, b) || (isBothNotNull(a, b) && isArray(a, b) && a.equals(b)));
+        return (isSameObject(a, b) || (isBothNotNull(a, b) && isArray(a, b) && a.equals(b)));
     }
 
     /**
@@ -152,30 +153,5 @@ public class DeepEqualsAndHashCode
         return true;
     }
 
-    /**
-     * Prüft ob beide Objekte nicht null sind.
-     *
-     * @param a Ein Objekt.
-     * @param b Ein anderes Objekt.
-     * @return True, wenn keines der beiden Objekte null ist.
-     */
-    private static boolean isBothNotNull(final Object a, final Object b)
-    {
-        return a != null && b != null;
-    }
 
-    /**
-     * Hasht alle Attribute innerhalb eines Objekts und generiert einen Hashcode nach den richtlinien der {@link Object#hashCode()} Methode.
-     * Implementiert durch {@link Arrays#deepHashCode(Object[])}.
-     *
-     * @param obj Das Objekt, wessen Hashcode generiert werden soll.
-     * @return Ein Hashcode basierend auf dem übergebenen Objekt.
-     */
-    public static int reflectiveHashCode(final Object obj)
-    {
-        ClassAccessor objectAccessor = new ClassAccessor(obj);
-        Map<String, Object> attributes = objectAccessor.getAllClassAttributes();
-        Object[] attributeValues = attributes.values().toArray();
-        return Arrays.deepHashCode(attributeValues);
-    }
 }
