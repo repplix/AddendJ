ccc636b1fb615ee9621e28effb9701e5143e47bb
repplix <michael.repplinger@icloud.com>
2020-03-07 08:14:01 +0100

    * Corrected comments

--- src/main/java/io/ddd/aspect/reflection/AttributeAccessException.java
+++ src/main/java/io/ddd/aspect/reflection/AttributeAccessException.java
@@ -1,17 +1,10 @@
 package io.ddd.aspect.reflection;
 
 /**
- * Diese Exception wird geworfen, falls es nicht möglich ist über Reflection auf ein
- * Attribut oder Feld einer Klasse zuzugreifen.
+ * Runtime exception which is used if we cannot access attributes of an object using reflection
  */
 public class AttributeAccessException extends RuntimeException
 {
-    /**
-     * Konstruktor
-     *
-     * @param message Die angezeigte Nachricht.
-     * @param cause   Der grund warum die Exception ausgelöst wurde
-     */
     public AttributeAccessException(final String message, final Throwable cause)
     {
         super(message, cause);
